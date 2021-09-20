02e58ddf2a0c7868129139f53f45dcb25938b67e
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-09-20 20:22:45 +0300

    add variant with context

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
 import * as React from 'react'
 import {Switch} from './switch'
 
+const ToggleContext = React.createContext()
+
+
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
     return     (<>
-    {React.Children.map(children, child => {
-      if (typeof child.type === 'string') return child
-      const newChild = React.cloneElement(child, {on, toggle})
-      return newChild
-    })}
+    
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
     <Switch on={on} onClick={toggle} />
     </>)
 }
 
 
-Toggle.On = ({children, on}) => <div>{on && children}</div>
+function ToggleOn ({children}) {
+  const {on} = React.useContext(ToggleContext)
+return <div>{on && children}</div>
+}
 
-Toggle.Off = ({children, on}) => <div>{!on && children}</div>
+function ToggleOff ({children}) {
+  const {on} = React.useContext(ToggleContext)
+return <div>{!on && children}</div>
+}
 
-Toggle.Button = ({toggle}) => <button onClick={toggle}>toggle too</button>
+function ToggleButton  () {
+  const {toggle} = React.useContext(ToggleContext)
 
+return <button onClick={toggle}>toggle too</button>
+}
 
 function App() {
   
   return (
     <div>
       <Toggle>
-        <Toggle.On>The button is on</Toggle.On>
-        <Toggle.Off>The button is off</Toggle.Off>
-       <Toggle.Button />
-       <div>Hello</div>
+        <ToggleOn>The button is on</ToggleOn>
+        <ToggleOff>The button is off</ToggleOff>
+       <div>
+          <ToggleButton />
+       </div>
       </Toggle>
 
     </div>
