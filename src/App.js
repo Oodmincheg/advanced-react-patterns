188944b2645c76371990b48d81c0cbe03a7b714e
Vorobiov Vladyslav <vorobyov.vladislav@gmail.com>
2021-09-20 20:00:07 +0300

    add compound components with children map

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import * as React from 'react'
 import {Switch} from './switch'
 
-function Toggle() {
+function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
-    return <Switch on={on} onClick={toggle} />
+    return     (<>
+    {React.Children.map(children, child => {
+      if (typeof child.type === 'string') return child
+      const newChild = React.cloneElement(child, {on, toggle})
+      return newChild
+    })}
+    <Switch on={on} onClick={toggle} />
+    </>)
 }
 
 
-const ToggleOn = () => null
+Toggle.On = ({children, on}) => <div>{on && children}</div>
 
-const ToggleOff = () => null
+Toggle.Off = ({children, on}) => <div>{!on && children}</div>
 
-const ToggleButton = () => null
+Toggle.Button = ({toggle}) => <button onClick={toggle}>toggle too</button>
 
 
 function App() {
+  
   return (
     <div>
       <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <ToggleButton />
+        <Toggle.On>The button is on</Toggle.On>
+        <Toggle.Off>The button is off</Toggle.Off>
+       <Toggle.Button />
+       <div>Hello</div>
       </Toggle>
+
     </div>
   )
 }
 
 export default App
-
-
